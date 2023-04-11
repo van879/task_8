166b5deaefbdc2b7d8a7e95eb8ac9103a9b9004f
Ivan Solomennikov <van879@rambler.ru>
2023-04-11 22:35:35 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace task_8
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int persons;
+			int timePerPerson = 10;
+			Console.WriteLine("сколько человек в очереди?");
+			persons = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine($"Вы должны отстоять в очереди {persons * timePerPerson / 60} часа и {persons * timePerPerson % 60} минут.");
+		}
+	}
+}
